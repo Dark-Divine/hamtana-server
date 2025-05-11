7d541b1f2becbc42f7215a21f1bf7f286df3dac7
Dark Divine <si9a.ir@gmail.com>
2025-05-11 12:02:31 -0700

    ✨ Refactor project structure and enhance TypeORM integration
    - Updated ESLint configuration to disable various TypeScript rules.
    - Simplified app module imports and added TypeORM and Config modules.
    - Enhanced base index files to export additional configurations and interfaces.
    - Introduced pagination and query interfaces for better data handling.
    - Implemented CRUD service with pagination support.
    - Added TypeORM configuration factory for dynamic database connection.
    - Created utility functions for data mapping and pagination.
    - Established a .dockerignore file for cleaner Docker builds.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,11 +25,33 @@ export default tseslint.config(
       },
     },
   },
+
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-explicit-any': 0,
+      '@typescript-eslint/no-misused-promises': 0,
+      '@typescript-eslint/no-unsafe-argument': 0,
+      '@typescript-eslint/explicit-function-return-type': 0,
+      '@typescript-eslint/no-namespace': 0,
+      '@typescript-eslint/parsers': 0,
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-use-before-define': [
+        'error',
+        { functions: false },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
     },
   },
 );
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,40 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { StoreModule } from './store/store.module';
-import { CourseModule } from './course/course.module';
-import { OrderModule } from './order/order.module';
-import { TicketModule } from './ticket/ticket.module';
-import { WalletModule } from './wallet/wallet.module';
-import { PaymentModule } from './payment/payment.module';
-import { NotificationModule } from './notification/notification.module';
-import { UploadModule } from './upload/upload.module';
-import { PostModule } from './post/post.module';
-import { SocialModule } from './social/social.module';
-import { I18nModule } from 'nestjs-i18n';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmConfigFactory } from './base';
+import typeorm from './base/configs/typeorm.config';
 
 @Module({
   imports: [
-    // I18nModule.forRoot({
-    //   fallbackLanguage: 'fa',
-    //   loaderOptions: {
-    //     path: path.join(__dirname, '/i18n/'),
-    //     watch: true,
-    //   },
-    // }),
+    ConfigModule.forRoot({ isGlobal: true, load: [typeorm] }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigFactory }),
 
     AuthModule,
     UserModule,
-    StoreModule,
-    CourseModule,
-    OrderModule,
-    TicketModule,
-    WalletModule,
-    PaymentModule,
-    NotificationModule,
-    UploadModule,
-    PostModule,
-    SocialModule,
+    // StoreModule,
+    // CourseModule,
+    // OrderModule,
+    // TicketModule,
+    // WalletModule,
+    // PaymentModule,
+    // NotificationModule,
+    // UploadModule,
+    // PostModule,
+    // SocialModule,
   ],
   controllers: [],
   providers: [],
